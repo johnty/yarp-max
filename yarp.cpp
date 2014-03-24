d7096aae56d9d1a889d3733577401e5fde0552fe
Johnty Wang <johntywang@gmail.com>
2014-03-23 22:20:42 -0700

    updated copyright notice

--- yarp.cpp
+++ yarp.cpp
@@ -1,12 +1,45 @@
-/**
-	@file
-	yarp		- yarp
-				- MaxMSP external with interfaces to yarp.
-				- requires libACE.dylib (OSX) or ACE(d).dll (Windows)
-	Johnty Wang -  johntywang@gmail.com
-	2014 - Hplus Canraie M+M project
+/*
+//--------------------------------------------------------------------------------------
+//
+//  File:       yarp.cpp
+//
+//  Project:    yarp-max
+//
+//  Contains:   max external with yarp interfaces
+//
+//  Written by: Johnty Wang
+//
+//  Copyright:  (c) 2014 by HPlus Technologies Ltd. and Simon Fraser University.
+//
+//              All rights reserved. Redistribution and use in source and binary forms,
+//              with or without modification, are permitted provided that the following
+//              conditions are met:
+//                * Redistributions of source code must retain the above copyright
+//                  notice, this list of conditions and the following disclaimer.
+//                * Redistributions in binary form must reproduce the above copyright
+//                  notice, this list of conditions and the following disclaimer in the
+//                  documentation and/or other materials provided with the
+//                  distribution.
+//                * Neither the name of the copyright holders nor the names of its
+//                  contributors may be used to endorse or promote products derived
+//                  from this software without specific prior written permission.
+//
+//              THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//              "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//              LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+//              PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+//              OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//              SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//              LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//              DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//              THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//              (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//              OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//  Created:    2014-01-17
+//
+//--------------------------------------------------------------------------------------
 */
-
 
 #undef check //for OSX
 
